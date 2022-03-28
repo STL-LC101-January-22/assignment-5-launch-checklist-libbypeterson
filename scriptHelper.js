11f8be38f9764f9779efeffee29780b63a6800a1
Libby Peterson <elibbypeterson@gmail.com>
2022-03-28 11:41:23 -0500

    trying to fetch json

--- scriptHelper.js
+++ scriptHelper.js
@@ -27,11 +27,15 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+     response.json().then(function(json) {
+         
+     });
 
-    return planetsReturned;
+    return planetsReturned
+});
 }
+
 
 function pickPlanet(planets) {
 }
